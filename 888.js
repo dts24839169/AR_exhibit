6ebe06cc9e8ac280a218792e5fd82073df2121ee
DTS <76682542+dts24839169@users.noreply.github.com>
2021-01-08 15:33:25 +0800

    Update 888.js

--- 888.js
+++ 888.js
@@ -7,15 +7,16 @@
                 xll:false,
                 ipad:false
             };
-          var p =navigator.platform;
-          system.win = p.indexof("win")== 0;
-          system.mac = p.indexof("mac")== 0;
-          system.xll = (p == "x11") || (p.indexof("linux") == 0);
-          system.ipad = (navigator.useragent.match(/ipad/i) !=null)?ture:false;
+          var p = navigator.platform;
+          system.win = p.indexOf("win")== 0;
+          system.mac = p.indexOf("mac")== 0;
+          system.xll = (p == "x11") || (p.indexOf("Linux") == 0);
+          system.ipad = (navigator.userAgent.match(/ipad/i) !=null)?ture:false;
           
-          if(system.win || system.mac || system.xll||system.ipad){
+          if(system.win || system.mac || system.xll || system.ipad){
+                   
           } else {
-               window.location = "222.html";
+               window.location.href = "http://google.com";
           }
 -->
           </script>
