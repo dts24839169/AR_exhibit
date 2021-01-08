46d4a58fd8724130d6a07e82096fd40e906c718e
DTS <76682542+dts24839169@users.noreply.github.com>
2021-01-08 15:36:23 +0800

    Update 888.js

--- 888.js
+++ 888.js
@@ -1,19 +1,19 @@
          <script type ="text/javascript">
   <!--       
-          <script> 
-            var system={
-                win:false,
-                mac:false,
-                xll:false,
-                ipad:false
-            };
+            var system = {
+                win: false,
+                mac: false,
+                xll: false,
+                ipad: false
+          };
+
           var p = navigator.platform;
-          system.win = p.indexOf("win")== 0;
-          system.mac = p.indexOf("mac")== 0;
+          system.win = p.indexOf("win") == 0;
+          system.mac = p.indexOf("mac") == 0;
           system.xll = (p == "x11") || (p.indexOf("Linux") == 0);
-          system.ipad = (navigator.userAgent.match(/ipad/i) !=null)?ture:false;
+          system.ipad = (navigator.userAgent.match(/ipad/i) != null)?true:false;
           
-          if(system.win || system.mac || system.xll || system.ipad){
+          if(system.win || system.mac || system.xll || system.ipad) {
                    
           } else {
                window.location.href = "http://google.com";
