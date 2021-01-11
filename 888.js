2b74a1430a72977dbb7220a04149ec5ccc108edc
DTS <76682542+dts24839169@users.noreply.github.com>
2021-01-11 09:53:20 +0800

    Update 888.js

--- 888.js
+++ 888.js
@@ -1,6 +1,6 @@
 <script type ="text/javascript">
                   
-function isMobile(){
+/*function isMobile(){
   return (
     (navigator.userAgent.match(/Android/i)) ||
     (navigator.userAgent.match(/webOS/i)) ||
@@ -13,6 +13,23 @@ function isMobile(){
 
 if(!isMobile()) {
 window.location = "222.html"
+}*/
+  
+  //重點是下面這行
+// true = 是手機，false 就是其他可能（電腦等等的）
+
+/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+
+//-------------------------------------------------------------------------------------------------------
+
+function is_mobile(){
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+if(is_mobile()){
+    window.location = "222.html";
+}else{
+    window.location = "404.html";
 }
 
 </script>
