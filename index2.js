4524478598b19720b2d8a08354df780f4bbcb1bc
DTS <76682542+dts24839169@users.noreply.github.com>
2021-01-18 14:52:36 +0800

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,19 +1,28 @@
-<script>
+<!DOCTYPE html>
+<html lang="en">
 
-(function() {
-  function(isAndroid() {
-    return( 
-    (navigator.userAgent.indexOf('Android')) > -1,
-    );
-    }
-    }  
+<head>
+       
+</head>
+
+<body>
+  
+</body>
+ 
+</html>
+
+<script>
+  (function() {
+     function isAndroid() {
+        return ( 
+         (navigator.userAgent.indexOf('Android')) > -1,
+     );
+   }
   
     if(isAndroid()) {
-        location.href = "googlechrome://navigate?url="https://dts24839169.github.io/wapaul-1/222.html";
+         location.href = 'googlechrome://navigate?url="https://dts24839169.github.io/wapaul-1/222.html;
     }else {
-        location.href = "googlechrome://"https://dts24839169.github.io/wapaul-1/222.html",
-    }
-    } 
-    }
-    
+         location.href = 'googlechrome://"https://dts24839169.github.io/wapaul-1/222.html;
+     }
+    })();
 </script>
