70d6c9a70671c61e50121cc316cfa040948fa0f9
DTS <76682542+dts24839169@users.noreply.github.com>
2021-01-19 10:39:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 <script>
   (function() {
-     function isAndroid() {
-        return ( 
-         (navigator.userAgent.indexOf('Android')) > -1
-  );
- }
-  
-    if(isAndroid()) {
-         location.href = 'googlechrome://navigate?url=dts24839169.github.io/wapaul-1/222.html';
-        
-    }else {
-         location.href = 'googlechrome://dts24839169.github.io/wapaul-1/222.html';
-     }
-    })();
-</script>
-
-<script>
-  (function() {
      function isIos() {
         return ( 
          (navigator.userAgent.match(/iPhone|iPad|iPod/i))
@@ -27,8 +10,7 @@
          location.href = 'googlechrome://dts24839169.github.io/wapaul-1/222.html';  
         
     }else {
-         //location.href = 'googlechrome://navigate?url=dts24839169.github.io/wapaul-1/222.html';
-         window.location = "googlechrome://navigate?url=google.com";   
+         location.href = 'googlechrome://navigate?url=dts24839169.github.io/wapaul-1/222.html';
     }
     })();
 </script>
