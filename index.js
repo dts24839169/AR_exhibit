bc1d7ee40a01d0a6631197e9a426347b984d9c68
DTS <76682542+dts24839169@users.noreply.github.com>
2021-01-19 10:58:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 <script>
   (function() {
+     thisUrl = location.hostname + location.pathname;  
      function isIos() {
         return ( 
          (navigator.userAgent.match(/iPhone|iPad|iPod/i))>-1
@@ -7,12 +8,17 @@
  }
   
     if( isIos()) {
-         location.href = 'googlechrome://dts24839169.github.io/wapaul-1/222.html';  
+         location.href = 'googlechrome://dts24839169.github.io/wapaul-1/222.html' + thisUrl;  
         
     }else {
-         location.href = 'googlechrome://navigate?url=dts24839169.github.io/wapaul-1/222.html';
+         location.href = 'googlechrome://navigate?url=dts24839169.github.io/wapaul-1/222.html' + thisUrl;
     }
     })();
+
+
+   // if (/Line/.test(navigator.userAgent)) {
+     //    location.href = location.href + ‘?openExternalBrowser=1’; }
+    
 </script>
 
 
